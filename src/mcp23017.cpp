b9fe5b6b9a15f4bfdc6336c7439237e8302bc319
Khiem Vu <kmvu2@wisc.edu>
2023-02-19 13:50:04 -0600

    finished rough prototype of driver-dev-tutorial

--- src/mcp23017.cpp
+++ src/mcp23017.cpp
@@ -15,86 +15,64 @@ Mcp23017::Mcp23017(int addr, I2C* i2cBus) {
 }
 
 uint8_t Mcp23017::get_dir(int pin) {
-    char buf[256] = {REGISTER_IODIRA};
+    char buf[1] = {REGISTER_IODIRA};
     
     this->i2cBus->write(this->addr, buf, 1, false);
-    this->i2cBus->read(this->addr, buf, 2, false);
+    this->i2cBus->read(this->addr, buf, 1, false);
 
-    uint16_t reg = 0;
-    reg |= buf[0];
-    reg <<= 8;
-    reg |= buf[1];
-
-    if ((reg && (1 >> pin)) > 0) {
-        return 1;
-    } else {
-        return 0;
-    }
+    return (buf[0] && (1 >> pin)) > 0;
 }
 
 
 // TODO: Read from state register
 uint8_t Mcp23017::get_state(int pin) {
-    char buf[256] = {REGISTER_GPIOA};
+    char buf[1] = {REGISTER_GPIOA};
     
     this->i2cBus->write(this->addr, buf, 1, false);
-    this->i2cBus->read(this->addr, buf, 2, false);
+    this->i2cBus->read(this->addr, buf, 1, false);
 
-    uint16_t reg = 0;
-    reg |= buf[0];
-    reg <<= 8;
-    reg |= buf[1];
-
-    if ((reg && (1 >> pin)) > 0) {
-        return 1;
-    } else {
-        return 0;
-    }
+    return (buf[0] && (1 >> pin)) > 0;
 }
 
 // TODO: Write to directions register
 int Mcp23017::set_dir(int pin, uint8_t dir) {
-    char buf[256] = {REGISTER_IODIRA, dir};
+    char buf[2] = {REGISTER_IODIRA, dir};
 
     this->i2cBus->write(this->addr, buf, 1, false);
-    this->i2cBus->read(this->addr, buf, 2, false);
+    this->i2cBus->read(this->addr, buf, 1, false);
 
-    uint16_t reg = 0;
-    reg |= buf[0];
-    reg <<= 8;
-    reg |= buf[1];
+    uint8_t data = buf[0];
 
-    char *data = 0;
-    if ((reg && (1 >> pin)) > 0) {
-        *data = 1;
+    if (dir) {
+        data |= 1 << pin;
     } else {
-        *data = 0;
+        data &= ~(1 << pin);
     }
 
-    this->i2cBus->write(this->addr, data, 3, false);
+    buf[0] = REGISTER_IODIRA;
+    buf[1] = data;
+    this->i2cBus->write(this->addr, buf, 2, false);
     return 0;
 }
 
 // TODO: Write to state register
 int Mcp23017::set_state(int pin, uint8_t val) {
-    char buf[256] = {REGISTER_GPIOA, val};
+    char buf[2] = {REGISTER_GPIOA, val};
 
     this->i2cBus->write(this->addr, buf, 1, false);
-    this->i2cBus->read(this->addr, buf, 2, false);
+    this->i2cBus->read(this->addr, buf, 1, false);
 
-    uint16_t reg = 0;
-    reg |= buf[0];
-    reg <<= 8;
-    reg |= buf[1];
+    uint8_t data = buf[0];
 
-    char *data = 0;
-    if ((reg && (1 >> pin)) > 0) {
-        *data = 1;
+    if (val) {
+        data |= 1 << pin;
     } else {
-        *data = 0;
+        data &= ~(1 << pin);
     }
 
-    this->i2cBus->write(this->addr, data, 3, false);
+    buf[0] = REGISTER_GPIOA;
+    buf[1] = data;
+    this->i2cBus->write(this->addr, buf, 2, false);
     return 0;
 }
 
@@ -103,7 +81,21 @@ int Mcp23017::set_state(int pin, uint8_t val) {
 int Mcp23017::begin(uint8_t directions[8]) {
     int rc;
 
+    char buf[1] = {REGISTER_IODIRA};
+    
+    this->i2cBus->write(this->addr, buf, 1, false);
+    this->i2cBus->read(this->addr, buf, 1, false);
+
+    uint8_t data = buf[0];
+
     // TODO: Add device ID check
+    if (data != 0xff) {
+        return 1;
+    }
+
+    for (int i = 0; i < 8; i++) {
+        set_dir(i, directions[i]);
+    }
 
     return 0;
 }

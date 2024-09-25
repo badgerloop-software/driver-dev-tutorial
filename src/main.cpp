54347c7b7bc38f751eeaae91a169fa2120566c30
Dhruv Aron <dhruv.aron@gmail.com>
2024-09-24 19:41:20 -0500

    My untested solution for the driver tutorial.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <Arduino.h>
 #include "mcp23017.h"
 
+#define address 
+
 // TODO: declare mcp23017 object
+Mcp23017 mcp(0x20);
 
 void setup() {
   // TODO: initialize i2c and mcp23017 object
+  Wire.begin();
+
+  uint8_t directions[8] = {1, 0, 0, 0, 0, 0, 0, 0};
+  mcp.begin(directions);
 }
 
 void loop() {
   // TODO: Write tests here
+  mcp.set_state(0, 1);
+  mcp.set_state(4, 1);
+  mcp.get_state(7);
+
+  delay(1000);
 }
--- src/mcp23017.cpp
+++ src/mcp23017.cpp
@@ -1,39 +1,84 @@
 #include "mcp23017.h"
 
 // TODO (optional): Define macros for useful register below:
-
+#define DIRECTIONS_REGISTER 0x00
+#define STATE_REGISTER 0x09
 
 // TODO: Initialize i2cBus member
 Mcp23017::Mcp23017(int addr) {
-
+   Mcp23017::addr = addr;
 }
 
 uint8_t Mcp23017::get_dir(int pin) {
-    return 0;
+   // specify what register to read from
+   Wire.requestFrom(DIRECTIONS_REGISTER, 1);
+
+   // read a byte from the specified register
+   uint8_t byte = Wire.read();
+
+   // return part of the byte that corresponds to the specific pin
+   return (byte >> pin) & 1;
 }
 
 
 // TODO: Read from state register
 uint8_t Mcp23017::get_state(int pin) {
-    return 0;
+   // specify what register to read from
+   Wire.requestFrom(STATE_REGISTER, 1);
+
+   // read a byte from the specified register
+   uint8_t byte = Wire.read();
+
+   // return part of the byte that corresponds to the specific pin
+   return (byte >> pin) & 1;
 }
 
 // TODO: Write to directions register
 int Mcp23017::set_dir(int pin, uint8_t dir) {
-    return 0;
+   Wire.requestFrom(DIRECTIONS_REGISTER, 1);
+   uint8_t prev = Wire.read();
+   uint8_t byte = (prev >> (pin + 1)) << (pin + 1);
+   byte += (dir << pin);
+   byte += (prev << (8 - pin)) >> (8 - pin);
+
+   Wire.beginTransmission(DIRECTIONS_REGISTER);
+   Wire.write(byte);
+   Wire.endTransmission();
+
+   return byte;
 }
 
 // TODO: Write to state register
 int Mcp23017::set_state(int pin, uint8_t val) {
-    return 0;
+   Wire.requestFrom(STATE_REGISTER, 1);
+   uint8_t prev = Wire.read();
+   uint8_t byte = (prev >> (pin + 1)) << (pin + 1);
+   byte += (val << pin);
+   byte += (prev << (8 - pin)) >> (8 - pin);
+
+   Wire.beginTransmission(STATE_REGISTER);
+   Wire.write(byte);
+   Wire.endTransmission();
+
+   return byte;
 }
 
 
 // Verifies that the device is accessible over I2C and sets pin directions
 int Mcp23017::begin(uint8_t directions[8]) {
-    int rc;
+   int rc;
 
-    // TODO: Add device ID check
+   // TODO: Add device ID check
+   Wire.requestFrom(DIRECTIONS_REGISTER, 1);
+   uint8_t byte = Wire.read();
+   if (!(byte ^ 0xFF)) {
+   return 0;
+   }
 
-    return 0;
+
+   for (int i = 0; i < 8; i++) {
+   set_dir(i, directions[i]);
+   }
+
+   return 1;
 }

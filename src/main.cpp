4d1f020969cb611d6ebd99deabf1b6a8c4caa0f8
Aditya YV <adityayv802@gmail.com>
2024-09-29 13:40:37 -0500

    my soln

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,32 @@
 #include "mcp23017.h"
 
 // TODO: declare mcp23017 object
+Mcp23017 mcp(0x20);
 
 void setup() {
   // TODO: initialize i2c and mcp23017 object
+  Wire.begin();
+  uint8_t dir[8] = {0, 0, 0, 0, 0, 1, 1, 1};
+  mcp.begin(dir);
+  Serial.begin(9600);
+  Serial.println("Hello World");
 }
 
 void loop() {
   // TODO: Write tests here
+  mcp.set_state(0, 0);
+  int a = mcp.get_state(0);
+  Serial.println(a);
+  mcp.set_state(4, 0);
+  a = mcp.get_state(4);
+  Serial.println(a);
+
+  delay(1000);
+
+  mcp.set_state(0, 1);
+  a = mcp.get_state(0);
+  Serial.println(a);
+  mcp.set_state(4, 1);
+  a = mcp.get_state(4);
+  Serial.println(a);
 }
--- src/mcp23017.cpp
+++ src/mcp23017.cpp
@@ -1,31 +1,96 @@
 #include "mcp23017.h"
 
 // TODO (optional): Define macros for useful register below:
-
+#define IODIRA 0x00
+#define IODIRB 0x01
+#define GPIOA 0x12
+#define GPIOB 0x13
 
 // TODO: Initialize i2cBus member
 Mcp23017::Mcp23017(int addr) {
-
+    this->addr = addr;
 }
 
 uint8_t Mcp23017::get_dir(int pin) {
-    return 0;
+    Wire.requestFrom(IODIRA, 1);
+    int w = 0;
+    if(Wire.available()){
+        w = Wire.read();
+    }
+    else{
+        return 0;
+    }
+    int p = 1 & (w >> pin);
+    return p;
 }
 
 
 // TODO: Read from state register
 uint8_t Mcp23017::get_state(int pin) {
-    return 0;
+    Wire.requestFrom(GPIOA, 1);
+    int w = 0;
+    if(Wire.available()){
+        w = Wire.read();
+    }
+    else{
+        return 0;
+    }
+    int p = 1 & (w >> pin);
+    return p;
 }
 
 // TODO: Write to directions register
 int Mcp23017::set_dir(int pin, uint8_t dir) {
-    return 0;
+    Wire.requestFrom(this->addr, 1);
+    int w = 0;
+        if (Wire.available()){
+        w = Wire.read();
+    }
+    else{
+        return 0;
+    }
+    if (dir == 1){
+        w = w | (1 << pin);
+    }
+    else{
+        w = w & ~(1 << pin);
+    }
+        
+    uint8_t data[2];
+    data[0] = IODIRA;
+    data[1] = w;
+    
+    Wire.beginTransmission(this->addr);
+    Wire.write(data, 2);
+    Wire.endTransmission();
+    return w;
 }
 
 // TODO: Write to state register
 int Mcp23017::set_state(int pin, uint8_t val) {
-    return 0;
+    Wire.requestFrom(this->addr, 1);
+    int w = 0;
+        if (Wire.available()){
+        w = Wire.read();
+    }
+    else{
+        return 0;
+    }
+    if (val == 1){
+        w = w | (1 << pin);
+    }
+    else{
+        w = w & ~(1 << pin);
+    }
+
+    uint8_t data[2];
+    data[0] = GPIOA;
+    data[1] = w;
+    
+    Wire.beginTransmission(this->addr);
+    Wire.write(data, 2);
+    Wire.endTransmission();
+    return w;
 }
 
 
@@ -34,6 +99,20 @@ int Mcp23017::begin(uint8_t directions[8]) {
     int rc;
 
     // TODO: Add device ID check
+    Wire.requestFrom(this->addr, 1);
+    if (Wire.available()){
+        rc = Wire.read();
+    }
+    else{
+        return 0;
+    }
+
+    int c = 0;
+
+    while (c < 8){
+        rc = set_dir(c, directions[c]);
+        c++;
+    }
 
     return 0;
 }

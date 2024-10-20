c3b85a68431b693409d919cd26406c747347cb75
Tristin Yun <53619342+Nexnar@users.noreply.github.com>
2024-10-19 23:57:21 -0500

    V2 main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <Arduino.h>
+al#include <Arduino.h>
 #include "mcp23017.h"
 
 // TODO: declare mcp23017 object
@@ -8,6 +8,8 @@ Mcp23017 mcp23017(0x20);
 void setup() {
   // TODO: initialize i2c and mcp23017 object
   Wire.begin();
+  uint8_t directions[8] = {1, 1, 1, 1, 1, 1, 0};
+  mcp23017.begin(directions);
   //Start serial
   Serial.begin(9600);
   Serial.println("Starting process...");
@@ -15,8 +17,11 @@ void setup() {
 
 void loop() {
   // TODO: Write tests here
-  uint8_t directions[8] = {1, 1, 1, 1, 1, 1, 0};
-  mcp23017.begin(directions);
-  mcp23017.get_state(0);
+  
+  //Toggles the state of the first pin and prints it
+  Serial.println(mcp23017.get_state(0));
   mcp23017.set_state(0, 1);
+  Serial.println(mcp23017.get_state(0));
+  mcp23017.set_state(0, 0);
+  delay(1000); //delay so it doesn't loop so quickly
 }

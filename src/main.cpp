1682050a1fcb5e1b07106baa9c41a89efd582bda
Tristin Yun <53619342+Nexnar@users.noreply.github.com>
2024-10-27 11:53:49 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ Mcp23017 mcp23017(0x20);
 void setup() {
   // TODO: initialize i2c and mcp23017 object
   Wire.begin();
-  uint8_t directions[8] = {1, 1, 1, 1, 1, 1, 1, 0};
+  uint8_t directions[8] = {0, 1, 1, 1, 1, 1, 1, 0};
   mcp23017.begin(directions);
   //Start serial
   Serial.begin(9600);

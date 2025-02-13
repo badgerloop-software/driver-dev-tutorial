8d8ef2a05aedfb9cff15376d563a623b47ae7412
jackkoestering <jkoestering@wisc.edu>
2025-02-12 18:59:53 -0600

    driver tutorial 2nd push

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "mcp23017.h"
 
 // TODO: declare mcp23017 object
-Mcp23017 mcp(0x20); // i2c address 
+Mcp23017 mcp(0x20); // i2c address, 0x01000000 (A2-A0)
 void setup() {
   // initialize Serial for printouts
   Serial.begin(115200);
@@ -11,7 +11,7 @@ void setup() {
 
   Wire.begin();
 
-  uint8_t directions[8] = {0, 1, 0, 1, 0, 1, 1, 0};
+  uint8_t directions[8] = {0, 0, 0, 0, 0, 1, 0, 0};
 
   if (mcp.begin(directions) != 0) 
   {
@@ -26,8 +26,8 @@ void setup() {
 void loop() {
   // TODO: Write tests here
 
-  // 1) set pin 0 HIGH
-  mcp.set_state(0, 1);
+  // 1) set pin 7 HIGH
+  mcp.set_state(7, 1);
   delay(500);
   uint8_t state = mcp.get_state(0);
   if (state == 1)
@@ -38,44 +38,25 @@ void loop() {
   {
     Serial.println("test 1 failed");
   }
-
-  // 2) set pin 1 LOW
-  mcp.set_state(1, 0);
+// set pin 7 low
+  mcp.set_state(7, 0);
   delay(500);
-  uint8_t state = mcp.get_state(1);
-  if (state == 0)
+  state = mcp.get_state(0);
+  if (state == 1)
   {
-    Serial.println("test 2 passed");
+    Serial.println("test 1 passed");
   }
   else
   {
-    Serial.println("test 2 failed");
+    Serial.println("test 1 failed");
   }
 
-  // 3) change direction of pin 0 to HIGH
-  mcp.set_dir(0, 1);
-  delay(500);
-  uint8_t dir = mcp.get_dir(1);
-  if (dir == 1) 
-  {
-    Serial.println("test 3 passed");
-  } else 
-  {
-    Serial.println("test 3 failed");
-  }
+  // 2) set pin 1 LOW
 
-  // 3) change direction of pin 1 to LOW
-  mcp.set_dir(1, 0);
-  delay(500);
-  uint8_t dir = mcp.get_dir(1);
-  if (dir == 1) 
-  {
-    Serial.println("test 4 passed");
-  } else 
-  {
-    Serial.println("test 4 failed");
-  }
+  state = mcp.get_state(5);
+
+  Serial.println(state);
+ 
 
 }
-
 
--- src/mcp23017.cpp
+++ src/mcp23017.cpp
@@ -18,7 +18,7 @@
 // stores current state of GPIO pins in port A
 // when pin is configured as input, GPIO reads its state: 0 (LOW, 0V) or 1 (HIGH, VCC)
 // when pin is configured as output, writing to GPIO controls its state
-#define GPIOA 0x12
+#define GPIOA_CLONE 0x12
 
 // TODO: Initialize i2cBus member
 // constructor
@@ -39,7 +39,7 @@ uint8_t Mcp23017::get_dir(int pin) {
     // end write operation to indicate we are done selecting the register
     Wire.endTransmission();
     // requst 1 byte of data from IODRIA register to determine port A pins I/O direction
-    Wire.requestFrom(addr, (uint8_t)1); 
+    Wire.requestFrom(addr, 1); 
     // received byte, I/O direction
     uint8_t iodir;
     // check if MCP23017 has sent data back over i2c
@@ -62,12 +62,12 @@ uint8_t Mcp23017::get_dir(int pin) {
 uint8_t Mcp23017::get_state(int pin) {
     // start i2c communication with MCP23017 at stored address
     Wire.beginTransmission(addr);
-    // select GPIOA register
-    Wire.write(GPIOA);
+    // select GPIOA_CLONE register
+    Wire.write(GPIOA_CLONE);
     // end write operation to indicate we are done selecting the register
     Wire.endTransmission();
-    // request 1 byte of data from GPIOA register to determine current state of port A pins
-    Wire.requestFrom(addr, (uint8_t)1);
+    // request 1 byte of data from GPIOA_CLONE register to determine current state of port A pins
+    Wire.requestFrom(addr, 1);
     // received byte, state of port A
     uint8_t gpio;
     // check if MCP23017 has sent data back over i2C
@@ -97,7 +97,7 @@ int Mcp23017::set_dir(int pin, uint8_t dir)
     // end write operation to indicate we are done selecting the register
     Wire.endTransmission();
     // request 1 byte of data from IODIRA register to determine current state of port A pins
-    Wire.requestFrom(addr, (uint8_t)1);
+    Wire.requestFrom(addr, 1);
     // received byte, state of port A
     uint8_t iodir;
     // check if MCP23017 has sent data back over i2c
@@ -127,7 +127,7 @@ int Mcp23017::set_dir(int pin, uint8_t dir)
     Wire.beginTransmission(addr);
     // select IODIRA register
     Wire.write(IODIRA);
-    // select modified GPIOA, new pin state
+    // select modified GPIOA_CLONE, new pin state
     Wire.write(iodir);
     // end the i2c transmission and sends data to MCP23017
     return Wire.endTransmission();
@@ -139,12 +139,12 @@ int Mcp23017::set_state(int pin, uint8_t val)
 {
     // start i2c communication with MCP23017 at stored address
     Wire.beginTransmission(addr);
-    // select GPIOA register
-    Wire.write(GPIOA);
+    // select GPIOA_CLONE register
+    Wire.write(GPIOA_CLONE);
     // end write operation to indicate we are done selecting the register
     Wire.endTransmission();
-    // request 1 byte of data from GPIOA register to determine current state of port A pins
-    Wire.requestFrom(addr, (uint8_t)1);
+    // request 1 byte of data from GPIOA_CLONE register to determine current state of port A pins
+    Wire.requestFrom(addr, 1);
     // received byte, state of port A
     uint8_t gpio;
     // check if MCP23017 has sent data back over i2c
@@ -173,9 +173,9 @@ int Mcp23017::set_state(int pin, uint8_t val)
 
     // start i2c communication with MCP23017 at stored address
     Wire.beginTransmission(addr);
-    // select GPIOA register
-    Wire.write(GPIOA);
-    // select modified GPIOA, new pin state
+    // select GPIOA_CLONE register
+    Wire.write(GPIOA_CLONE);
+    // select modified GPIOA_CLONE, new pin state
     Wire.write(gpio);
     // end the i2c transmission and sends data to MCP23017
     return Wire.endTransmission();
@@ -193,7 +193,7 @@ int Mcp23017::begin(uint8_t directions[8])
     // end write operation to indicate we are done selecting the register
     Wire.endTransmission();
     // request 1 byte of data from IODIRA register to determine current state of port A pins
-    Wire.requestFrom(addr, (uint8_t)1);
+    Wire.requestFrom(addr, 1);
     // received byte, state of port A
     uint8_t iodir_value;
     // check if MCP23017 has sent data back over i2c
@@ -218,11 +218,17 @@ int Mcp23017::begin(uint8_t directions[8])
             iodir |= (1 << i); 
         }
     }
+
+    if (iodir != iodir_value) 
+    {
+        return -1;
+    }
+
     // start i2c communication with MCP23017 at stored address
     Wire.beginTransmission(addr);
     // select IODIRA register
     Wire.write(IODIRA);
-    // select modified GPIOA, new pin state
+    // select modified GPIOA_CLONE, new pin state
     Wire.write(iodir);
     // end the i2c transmission and sends data to MCP23017
     return Wire.endTransmission();

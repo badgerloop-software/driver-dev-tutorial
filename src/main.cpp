00341fa776ba37161a0ebd1962c6c311dc3a4a7e
Porter Fencl <pfencl@wisc.edu>
2025-09-29 18:14:59 -0500

    finished new member task 1

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 #endif
 
 // TODO: declare mcp23017 object
-
+Mcp23017 obj1(0x20);
 void setup() {
   // initialize Serial for printouts
   Serial.begin(115200);
@@ -23,8 +23,17 @@ void setup() {
     //Make sure to check if it is an Active-High or Active-Low reset
   #endif
   // TODO: initialize I2C and mcp23017 object
+  Wire.begin();
+  uint8_t numbers[8] = {0, 0, 0, 0, 0, 1, 0, 0};
+  obj1.begin(numbers);
 }
 
 void loop() {
   // TODO: Write tests here
+  obj1.set_state(7, 1);
+  Serial.println(obj1.get_state(5));
+  delay(1000);
+  obj1.set_state(7, 0);
+  delay(1000);
+  Serial.println(obj1.get_state(5));
 }

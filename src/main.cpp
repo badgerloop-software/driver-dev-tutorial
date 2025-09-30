2bce9c95f3a5db09c9deed92f04ac4b6ebebff49
germilla <87410524+Mightyman14386@users.noreply.github.com>
2025-09-29 21:16:33 -0500

    wtf

--- src/main.cpp
+++ src/main.cpp
@@ -12,19 +12,36 @@
 #endif
 
 // TODO: declare mcp23017 object
+Mcp23017 controller(0x20);
 
 void setup() {
   // initialize Serial for printouts
   Serial.begin(115200);
   printf("Test begins\n"); 
 
+ 
+
+
   #ifdef EXTERNAL_RESET_ENABLED
     //TODO: Configure the digital pin acting as a reset an output pin and reset the IOExpander registers to their default values.
     //Make sure to check if it is an Active-High or Active-Low reset
   #endif
   // TODO: initialize I2C and mcp23017 object
+  uint8_t setup[8] = {1, 1, 1, 1, 1, 1, 1, 0};
+  int status = controller.begin(setup);
+  if (status) {
+    Serial.println("kys");
+  }
+  else
+    Serial.println("chat we cookin?");
 }
 
 void loop() {
   // TODO: Write tests here
+  int switchVal = controller.get_state(5);
+  controller.set_state(7, 1);
+  delay(500);
+  controller.set_state(7, 0);
+  Serial.printf("Switch value : %d \n", switchVal);
+  delay(500);
 }
--- src/mcp23017.cpp
+++ src/mcp23017.cpp
@@ -1,39 +1,87 @@
 #include "mcp23017.h"
 
 // TODO (optional): Define macros for useful register below:
-
+#define IODIRA 0x00
+#define GPIOA 0x12
+int addr;
 
 // TODO: Initialize i2cBus member
 Mcp23017::Mcp23017(int addr) {
-
+    this->addr = addr;
+    Wire.begin();
 }
 
 uint8_t Mcp23017::get_dir(int pin) {
-    return 0;
+    Wire.beginTransmission(addr);
+    Wire.write(IODIRA);
+    Wire.endTransmission();
+    Wire.requestFrom(addr, 1);
+    uint8_t byte = Wire.read();
+    int bit = (byte >> pin) & 1;
+    return bit;
 }
 
 
-// TODO: Read from state register
 uint8_t Mcp23017::get_state(int pin) {
-    return 0;
+    Wire.beginTransmission(addr);
+    Wire.write(GPIOA);
+    Wire.endTransmission();
+    Wire.requestFrom(addr, 1);
+    uint8_t byte = Wire.read();
+    int bit = (byte >> pin) & 1;
+    return bit;
 }
 
 // TODO: Write to directions register
 int Mcp23017::set_dir(int pin, uint8_t dir) {
+    Wire.beginTransmission(addr);
+    Wire.write(IODIRA);
+    Wire.endTransmission();
+    Wire.requestFrom(addr, 1);
+    uint8_t byte = Wire.read();
+    byte = (byte & ~(1 << pin)) | (dir << pin);
+    Serial.printf("okokok %d \n", byte);
+    Wire.beginTransmission(addr);
+    Wire.write(IODIRA);
+    Wire.write(byte);
+    Wire.endTransmission();
     return 0;
 }
 
 // TODO: Write to state register
 int Mcp23017::set_state(int pin, uint8_t val) {
+    Wire.beginTransmission(addr);
+    Wire.write(GPIOA);
+    Wire.endTransmission();
+    Wire.requestFrom(addr, 1);
+    uint8_t byte = Wire.read();
+    byte = (byte & ~(1 << pin)) | (val << pin);
+    Wire.beginTransmission(addr);
+    Wire.write(GPIOA);
+    Wire.write(byte);
+    Wire.endTransmission();
     return 0;
 }
 
 
 // Verifies that the device is accessible over I2C and sets pin directions
 int Mcp23017::begin(uint8_t directions[8]) {
-    int rc;
 
+    // Read IODIRA and print the byte stored in it
+
+    Wire.beginTransmission(addr);
+    Wire.write(IODIRA);
+    Wire.endTransmission();
+    Wire.requestFrom(addr, 1);
+    uint8_t byte = Wire.read();
+    Serial.printf("%d \n", byte);
+
+
+    int state;
+    for (int i = 0; i < 8; i++) {
+        state = set_dir(i, directions[i]);
+        if (state)
+            return 1;
+    }
     // TODO: Add device ID check
-
-    return 0;
 }
